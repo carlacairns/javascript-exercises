8657c4cf8733a7a822c5200a03be2572ce87b319
Carla <ccairns1999@gmail.com>
2020-12-02 17:11:21 -0600

    All tests pass

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,28 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseString = '';
+    let array = [];
+    let reverseArray = [];
+    let finalString =  '';
+    if (string.length == 1 || string.length == 0){
+        return string;
+    }
+    for (let i = 0; i < string.length; i++) { 
+        array[i] = string.charAt(i);  // making string into array
+    } 
+    
+    reverseArray = array.reverse();
+    reverseString = reverseArray.toString();
+    
+   loop: for (let i = 0; i < reverseString.length; i++){
+        if (reverseString.charAt(i) == ','){
+            continue loop;
+        }
+        else {
+            finalString += reverseString.charAt(i);
+        }
+    }
+
+    return finalString;
 
 }
 
