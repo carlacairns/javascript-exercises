307eb0cdd9d5bde1512c4ea45a835ba14275654e
Carla <ccairns1999@gmail.com>
2020-12-02 13:23:56 -0600

    all tests pass

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeatString = "";
+    let negativeTest = number.toString();
+    if (negativeTest.charAt(0) == '-'){
+        return "ERROR"
+    }
+    for (let i = 0; i < number; i++ ){
+        repeatString += string;
+    }
+    return repeatString;
 }
 
 module.exports = repeatString
