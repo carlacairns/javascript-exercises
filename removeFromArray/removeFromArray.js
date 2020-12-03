23b483766337487caf13fd174d32cef6c7b07bdc
Carla <ccairns1999@gmail.com>
2020-12-03 10:23:36 -0600

    all tests passing

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+    const removeFromArray = function() {
+        let array = Array.from(arguments)
+        let remove = [];
 
-}
+        array = array[0]; // array separated from removal data
+        for (let i = 0; i < arguments.length; i++){ //pushing all arguments into removal
+            remove.push(arguments[i]);
+        }
+        remove.splice(0,1); // removing array portion
+        remove.sort(); 
+        // must be added because index may become obolete as we splice out the index in the array
+        //see test 2
+        
+        for (let i = 0; i < array.length; i++){
+            for (let j = 0; j < remove.length; j++){
+                if (array[i] === remove[j]){
+                    
+                    array.splice(i, 1); // removes unwanted data
+                }
+            }
+        }
+        
+    
+        return array;
+        
+    }
+
+
 
 module.exports = removeFromArray
